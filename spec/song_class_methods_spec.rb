7115afeaddf1a6a1a35be5c7a937f454d3ce34f9
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-15 19:31:33 +0000

    Automatically backed up by Learn

--- spec/song_class_methods_spec.rb
+++ spec/song_class_methods_spec.rb
@@ -77,10 +77,10 @@ describe "Song Class Methods" do
 
   describe '.new_from_filename' do
     it 'initializes a song and artist_name based on the filename format' do
-      song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
+      song = Song.new_from_filename("Foo Fighters - Everlong.mp3")
 
-      expect(song.name).to eq("For Love I Come")
-      expect(song.artist_name).to eq("Thundercat")
+      expect(song.name).to eq("Everlong")
+      expect(song.artist_name).to eq("Foo Fighters")
     end
   end
 
