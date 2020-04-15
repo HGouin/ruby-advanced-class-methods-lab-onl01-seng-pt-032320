db5bdbeb4080ef4c8cc3b8a3a0cf2097ae67bf12
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-15 18:30:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,46 @@ class Song
     self.class.all << self
   end
 
+  def Song.create
+    @song = self.new
+    @song.save
+    @song
+  end
+
+  def Song.new_by_name(name)
+  @song = self.new
+    @song.name = name
+    @song
+  end
+
+  def Song.create_by_name(name)
+    name = self.new
+    name.name = name
+    @@all << name
+    end
+
+    def Song.find_by_name(name)
+
+    end
+
+    def Song.find_or_create_by_name(name)
+    end
+
+    def Song.alphabetical
+    end
+
+    def Song.new_from_filename
+    end
+
+    def Song.create_from_filename
+    end
+
+    def Song.destroy_all
+      self.all.clear
+    end
+
+
+
+
+
 end
